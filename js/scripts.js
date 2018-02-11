ddc65f3d1eb0dd869cfe8c43113ea981c1352a9f
ahuscia <a.huscia@gmail.com>
2018-02-11 15:00:42 +0100

    Add script.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+function drawTree(a) {
+    for (var i = 0; i < a; i++) {
+            var star = '';
+            for (var j = 0; j <= i; j++) {
+                var star = star + '*';
+            }
+        console.log(star);
+        }
+    }
